4d08ae9085f196491855ee0bd7573f5ddd28b748
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2025-09-07 18:55:48 -0500

    Implementacion de token unico con expiracion
    * Implementa autorizacion con JWT + verificacion de rol
    
    * password forgot, genera un token unico con expiracion

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,9 +12,11 @@ app.get("/", (req, res) => {
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 const taskRoutes = require("./routes/tasks");
+const passwordRoutes = require("./routes/password");
 
 app.use("/api", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
+app.use("/api/password", passwordRoutes);
 
 module.exports = app;
